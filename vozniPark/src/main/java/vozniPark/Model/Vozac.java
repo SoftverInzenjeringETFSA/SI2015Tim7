8001747d7cb1327bf851148cc6c2ba3b9ef026b9
IrmaKarasoftic <ikarasofti1@etf.unsa.ba>
2016-05-08 17:43:04 +0200

    Implementacija - Vozac

--- vozniPark/src/main/java/vozniPark/Model/Vozac.java
+++ vozniPark/src/main/java/vozniPark/Model/Vozac.java
@@ -8,6 +8,8 @@ public class Vozac extends Osoba implements java.io.Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private String brojVozacke;
+	private String username;
+	private String password;
 
 	public String getBrojVozacke() {
 		return brojVozacke;
@@ -15,6 +17,22 @@ public class Vozac extends Osoba implements java.io.Serializable{
 
 	public void setBrojVozacke(String brojVozacke) {
 		this.brojVozacke = brojVozacke;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 	
 	public Vozac(){}
