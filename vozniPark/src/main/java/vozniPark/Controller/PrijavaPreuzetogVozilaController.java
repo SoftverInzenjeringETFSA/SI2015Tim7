fd300d8a3f54fc19763b43bb1d50bfad13d2dde7
IrmaKarasoftic <ikarasofti1@etf.unsa.ba>
2016-05-21 03:37:49 +0200

    Fixed issue #92

--- vozniPark/src/main/java/vozniPark/Controller/PrijavaPreuzetogVozilaController.java
+++ vozniPark/src/main/java/vozniPark/Controller/PrijavaPreuzetogVozilaController.java
@@ -10,6 +10,7 @@ import java.util.Vector;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 import vozniPark.Util.*;
 import vozniPark.Model.*;
@@ -62,32 +63,41 @@ public class PrijavaPreuzetogVozilaController {
 		for(int i=0; i< listaVozila.size(); i++) 
 		{
 			if(listaVozila.get(i).getRegistracija().contentEquals(registracija))
-			{	
-				long id = listaVozila.get(i).getId();
-				Session session = HibernateUtil.getSessionFactory().openSession();
-				Transaction t = session.beginTransaction();
-				Vozilo v = (Vozilo) session.load(Vozilo.class, id);
-				v.setStatus("Zauzet");
-				session.save(v);
-				
-				
-				Voznje voznje = new Voznje();
-				SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy 'u' HH:mm:ss");
-				String datumVrijeme = datum + vrijeme;
-				Date date = new Date();
-				try {
-					date = formatter.parse(datumVrijeme);
-				} catch (ParseException e) {
-					// TODO Auto-generated catch block
-					//e.printStackTrace();
-					logger.info(e);
+			{
+				if(listaVozila.get(i).getStatus()=="Zauzet")
+				{
+					JOptionPane.showMessageDialog(null, "Automobil je zauzet");
+					return;
 				}
-				voznje.setDatumPreuzimanja(date);
-				voznje.setVozilo(v);			
-				
-				session.save(voznje);
-				
-				t.commit();
+				else {
+					long id = listaVozila.get(i).getId();
+					Session session = HibernateUtil.getSessionFactory().openSession();
+					Transaction t = session.beginTransaction();
+					Vozilo v = (Vozilo) session.load(Vozilo.class, id);
+					v.setStatus("Zauzet");
+					session.save(v);
+					
+					
+					Voznje voznje = new Voznje();
+					SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy 'u' HH:mm:ss");
+					String datumVrijeme = datum + vrijeme;
+					Date date = new Date();
+					try {
+						date = formatter.parse(datumVrijeme);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						//e.printStackTrace();
+						logger.info(e);
+					}
+					voznje.setDatumPreuzimanja(date);
+					voznje.setVozilo(v);			
+					
+					session.save(voznje);
+					
+					t.commit();
+					JOptionPane.showMessageDialog(null, "Automobil je uspješno prijavljen");
+					return;
+				}
 			}
 		}
 	}
